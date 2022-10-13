6dac449fbf322cc7e171cfe6bcba0f8562b00586
Pekka Kanerva <84311161+pekka-kanerva@users.noreply.github.com>
2022-10-13 16:25:11 +0300

    Create ff-week14-solution.sql

---
+++ week14/ff-week14-solution.sql
@@ -0,0 +1,36 @@
+-- Week 14 challenge
+
+-- Use Frosty Friday role created in week 1 
+use role ff_role;
+
+-- Use database for challenges created in week 1
+use database frosty_friday;
+
+-- Set warehouse
+use warehouse compute_wh;
+
+-- Create schema for week13 challenge
+create schema week14;
+
+-- Create table as given in challenge
+CREATE OR REPLACE TABLE week_14 (
+    superhero_name varchar(50),
+    country_of_residence varchar(50),
+    notable_exploits varchar(150),
+    superpower varchar(100),
+    second_superpower varchar(100),
+    third_superpower varchar(100)
+);
+
+INSERT INTO week_14 VALUES ('Superpig', 'Ireland', 'Saved head of Irish Farmer\'s Association from terrorist cell', 'Super-Oinks', NULL, NULL);
+INSERT INTO week_14 VALUES ('Señor Mediocre', 'Mexico', 'Defeated corrupt convention of fruit lobbyists by telling anecdote that lasted 33 hours, with 16 tangents that lead to 17 resignations from the board', 'Public speaking', 'Stamp collecting', 'Laser vision');
+INSERT INTO week_14 VALUES ('The CLAW', 'USA', 'Horrifically violent duel to the death with mass murdering super villain accidentally created art installation last valued at $14,450,000 by Sotheby\'s', 'Back scratching', 'Extendable arms', NULL);
+INSERT INTO week_14 VALUES ('Il Segreto', 'Italy', NULL, NULL, NULL, NULL);
+INSERT INTO week_14 VALUES ('Frosty Man', 'UK', 'Rescued a delegation of data engineers from a DevOps conference', 'Knows, by memory, 15 definitions of an obscure codex known as "the data mesh"', 'can copy and paste from StackOverflow with the blink of an eye', NULL);
+
+
+-- Solution
+select to_json(object_construct( 'country_of_residence', t.country_of_residence
+                               , 'superhero_name', t.superhero_name 
+                               , 'superpowers', array_construct_compact(t.superpower, t.second_superpower, t.third_superpower)))
+from week_14 t;
